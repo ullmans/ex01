f6fa9405bc0132e426f54da730d522686b609583
ullmans <shilo.ullman@gmail.com>
2020-08-13 20:46:44 +0300

    added matrix_create function

--- Matrix.c
+++ Matrix.c
@@ -1,6 +1,64 @@
 #include <stdio.h>
+#include "Matrix.h"
 
-int main(){
-    printf("hi\n");
-    return 0;
+typedef struct Matrix {
+    uint32_t height;
+    uint32_t width;
+    int** values;
+} Matrix;
+
+const uint32_t FIRST_POSSIBLE_VALUE = 0;
+
+
+ErrorCode matrix_create(PMatrix* matrix, uint32_t height, uint32_t width){
+    //check if wisth and height are valid
+    if(width < FIRST_POSSIBLE_VALUE || height < FIRST_POSSIBLE_VALUE){
+        return ERROR_NEG_WIDTH_OR_HEIGHT;
+    }
+
+    //allocate memory for the matrix
+    *matrix = (PMatrix) malloc(sizeof(Matrix));
+    if(matrix = NULL){
+        return ERROR_FAILED_MEMORY_ALOCATION;
+    }
+    (*matrix)->height = height;
+    (*matrix)->width = width;
+
+    //allocate memory for the array of the arrays
+    (*matrix)->values =  (double**)malloc (height * sizeof(double*));
+    if((*matrix)->values = NULL){
+        return ERROR_FAILED_MEMORY_ALOCATION;
+    }
+    
+    //allocate memory for the arrays insude the array
+    for (uint32_t i = 0; i < height; ++i){
+        ((*matrix)->values)[i] = (double*) calloc (width, sizeof(double));
+        if(((*matrix)->values)[i] = NULL){
+            return ERROR_FAILED_MEMORY_ALOCATION;
+        }
+    }
+
+    return ERROR_SUCCESS;
 }
+
+//ErrorCode matrix_copy(PMatrix* result, CPMatrix source);
+
+//void matrix_destroy(PMatrix matrix);
+
+//ErrorCode matrix_getHeight(CPMatrix matrix, uint32_t* result);
+
+//ErrorCode matrix_getWidth(CPMatrix matrix, uint32_t* result);
+
+//ErrorCode matrix_setValue(PMatrix matrix, uint32_t rowIndex, uint32_t colIndex, double value);
+
+//ErrorCode matrix_getValue(CPMatrix matrix, uint32_t rowIndex, uint32_t colIndex, double* value);
+
+//ErrorCode matrix_add(PMatrix* result, CPMatrix lhs, CPMatrix rhs);
+
+//ErrorCode matrix_multiplyMatrices(PMatrix* result, CPMatrix lhs, CPMatrix rhs);
+
+//ErrorCode matrix_multiplyWithScalar(PMatrix matrix, double scalar);
+
+
+
+
