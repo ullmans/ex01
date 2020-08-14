a1eef24080ae3c7a439765a613ae5c3d5759eb4d
ullmans <shilo.ullman@gmail.com>
2020-08-14 13:19:14 +0300

    change the functions to know the new errors

--- ErrorCode.c
+++ ErrorCode.c
@@ -12,11 +12,8 @@ const char* error_getErrorMessage(ErrorCode code){
     switch (code)
     {
     case ERROR_SUCCESS: return "the operation has promoted successfully";
-    case ERROR_MISSING_MATRIX: return "input matrix pointer is null";
-    case ERROR_MISSING_MATRIX_HEIGHT: return "has not matrix height";
-    case ERROR_MISSING_MATRIX_WIDTH: return "has not matrix width";
-    case ERROR_MISSING_MATRIX_VALUES: return "missing values from the matrix";
-    case ERROR_NULL_OUTPUT_POINTER: return "try to store info in NULL adress";
+    case ERROR_NULL_POINTER: return "null pointer exeption";
+    case ERROR_CAN_NOT_MULTI: return "the two matrixs cant be multiple together in this order";
     case ERROR_FAILED_MEMORY_ALOCATION: return "couldn't alloctae memory";
     case ERROR_NEG_WIDTH_OR_HEIGHT: return "negative width or height";
     default: return "";
