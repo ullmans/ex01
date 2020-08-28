e8c658e51b758a417dcf8a71c93711b6c2886e98
ullmans <shilo.ullman@gmail.com>
2020-08-28 10:20:14 +0300

    remove bad code and bad comments

--- src/Matrix.c
+++ src/Matrix.c
@@ -8,20 +8,15 @@ typedef struct Matrix {
     double** values;
 } Matrix;
 
-const uint32_t FIRST_POSSIBLE_VALUE = 0;
-
-
 ErrorCode matrix_create(PMatrix* matrix, uint32_t height, uint32_t width){
     //check if wisth and height are valid
-    if(width < FIRST_POSSIBLE_VALUE + 1 || height < FIRST_POSSIBLE_VALUE + 1){
+    if(width < 1 || height < 1){
         return ERROR_NEG_WIDTH_OR_HEIGHT;
     }
 
     if(matrix == NULL){
         return ERROR_NULL_POINTER;
     }
-
-    *matrix = NULL;
 
     //allocate memory for the matrix
     *matrix = (PMatrix) malloc(sizeof(Matrix));
@@ -66,9 +61,6 @@ ErrorCode matrix_copy(PMatrix* result, CPMatrix source){
     for(uint32_t i = 0; i < source->height; ++i){
         for(uint32_t j = 0; j < source->width; ++j){
             matrix_setValue(*result, i, j, (source->values)[i][j]);
-            // double* newVal = 0;
-            // matrix_getValue(source, i, j, newVal);
-            // matrix_setValue(*result, i, j, *newVal);
         }
     }
     return ERROR_SUCCESS;
@@ -127,35 +119,29 @@ ErrorCode matrix_getValue(CPMatrix matrix, uint32_t rowIndex, uint32_t colIndex,
     
     //check for errors
     if(matrix == NULL){
-        //printf("matrix_getValue_1\n");
         return ERROR_NULL_POINTER;
     }
 
-    if(rowIndex < FIRST_POSSIBLE_VALUE || colIndex < FIRST_POSSIBLE_VALUE){
-        //printf("matrix_getValue_2\n");
+    if(rowIndex < 0 || colIndex < 0){
         return ERROR_NEG_WIDTH_OR_HEIGHT;
     }
 
     if(value == NULL){
-        //printf("matrix_getValue_3\n");
         return ERROR_NULL_POINTER;
     }
 
     for(uint32_t i = 0; i < matrix->height; ++i){
         if(matrix->values[i] == NULL){
-            //printf("matrix_getValue_4\n");
             return ERROR_NULL_POINTER;
         }
     }
 
     *value = (matrix->values)[rowIndex][colIndex];
-    //printf("matrix_getValue_5\n");
     return ERROR_SUCCESS;
 }
 
 ErrorCode matrix_add(PMatrix* result, CPMatrix lhs, CPMatrix rhs){
     if (lhs == NULL || rhs == NULL){
-        //printf("matrix_add_1\n");
         return ERROR_NULL_POINTER;
     }
 
@@ -168,18 +154,15 @@ ErrorCode matrix_add(PMatrix* result, CPMatrix lhs, CPMatrix rhs){
             matrix_setValue(*result, i, j, lhs->values[i][j] + rhs->values[i][j]);
         }
     }
-    //printf("matrix_add_3\n");
     return ERROR_SUCCESS;
 }
 
 ErrorCode matrix_multiplyMatrices(PMatrix* result, CPMatrix lhs, CPMatrix rhs){
     if (lhs == NULL || rhs == NULL){
-        //printf("matrix_multiplyMatrices_1\n");
         return ERROR_NULL_POINTER;
     }
 
     if(lhs->width != rhs->height){
-         //printf("matrix_multiplyMatrices_2\n");
         return ERROR_CAN_NOT_MULTI;
     }
 
@@ -187,27 +170,18 @@ ErrorCode matrix_multiplyMatrices(PMatrix* result, CPMatrix lhs, CPMatrix rhs){
     matrix_create(result, lhs->height, rhs->width);
 
     if(result == NULL){
-        //printf("matrix_multiplyMatrices_3\n");
         return ERROR_NULL_POINTER;
     }
 
     for(uint32_t i = 0; i < (*result)->height; ++i){
-        //printf("aaaaaa\n");
         for (uint32_t j = 0; j < (*result)->width; ++j){
-            //printf("bbbbb\n");
             double calculator = 0;
             for(uint32_t k = 0; k < lhs->width; ++k){
                 calculator += lhs->values[i][k] * rhs->values[k][j];
-                // double val1;
-                // double val2;
-                // matrix_getValue(lhs, i, k, &val1);
-                // matrix_getValue(rhs, k, j, &val2);
-                // calculator += (val1 * val2);
             }
             matrix_setValue(*result, i, j, calculator);
         }
     }
-    //printf("matrix_multiplyMatrices_5");
     return ERROR_SUCCESS;
 }
 
@@ -225,9 +199,5 @@ ErrorCode matrix_multiplyWithScalar(PMatrix matrix, double scalar){
     }
 
     return ERROR_SUCCESS;
-
+    
 }
-
-
-
-
