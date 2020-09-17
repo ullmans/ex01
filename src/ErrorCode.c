818b67ecf85df26060a62f196c0ec6e9bfd87a92
ullmans <shilo.ullman@gmail.com>
2020-09-17 11:42:36 +0300

    changed some of the errors

--- src/ErrorCode.c
+++ src/ErrorCode.c
@@ -10,9 +10,10 @@ const char* error_getErrorMessage(const ErrorCode code){
     {
     case ERROR_SUCCESS: return "the operation has promoted successfully";
     case ERROR_NULL_POINTER: return "null pointer exeption";
-    case ERROR_CAN_NOT_MULTI: return "the two matrixs cant be multiple together in this order";
+    case ERROR_CAN_NOT_MULTIPLY: return "the two matrixs cant be multiply together in this order";
     case ERROR_FAILED_MEMORY_ALOCATION: return "couldn't alloctae memory";
-    case ERROR_NEG_WIDTH_OR_HEIGHT: return "negative width or height";
+    case ERROR_NEGATIVE_WIDTH: return "negative width";
+    case ERROR_NEGATIVE_HEIGHT: return "negative height";
     default: return "";
     }
 }
